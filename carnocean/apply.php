d689282ea247572af2bcd6e21ae77e1a7064861b
Jatin Tiwari <myemail>
2022-12-09 03:28:10 +0530

    adding subsea7 website

---
+++ carnocean/apply.php
@@ -0,0 +1,417 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Carnocean Shipyards & Cruise Management India Pvt Ltd.</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+
+        <!-- Favicon -->
+        <link rel="icon" href="favicon.ico" type="image/x-icon">
+        <meta property="og:image" content="https://skyhaamburg.com/logo.PNG">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800&family=Rubik:wght@400;500;600;700&display=swap"
+        rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <!-- Spinner Start -->
+    <div id="spinner"
+        class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner"></div>
+    </div>
+    <!-- Spinner End -->
+
+    <!-- Navbar Start -->
+    <div class="container-fluid position-relative p-0">
+        <nav class="navbar navbar-expand-lg navbar-dark px-5 py-3 py-lg-0">
+            <a href="index.html" class="navbar-brand p-0">
+                <h1 class="m-0"><i class="fa fa-user-tie me-2"></i> Carnocean</h1>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                <span class="fa fa-bars"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <div class="navbar-nav ms-auto py-0">
+                    <a href="index.html" class="nav-item nav-link">Home</a>
+                    <a href="apply.html" class="nav-item nav-link active">Apply Now</a>
+                    <a href="about.html" class="nav-item nav-link">About Us</a>
+                    <a href="refund.html" class="nav-item nav-link">Refund</a>
+                    <a href="contact.html" class="nav-item nav-link">Contact</a>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container-fluid bg-primary py-5 bg-header" style="margin-bottom: 90px;">
+            <div class="row py-5">
+                <div class="col-12 pt-lg-5 mt-lg-5 text-center">
+                    <h1 class="display-4 text-white animated zoomIn">Apply Now</h1>
+                    <a href="/" class="h5 text-white">Home</a>
+                    <i class="far fa-circle text-white px-2"></i>
+                    <a href="" class="h5 text-white">Apply</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Navbar End -->
+
+
+    <!-- Full Screen Search Start -->
+    <div class="modal fade" id="searchModal" tabindex="-1">
+        <div class="modal-dialog modal-fullscreen">
+            <div class="modal-content" style="background: rgba(9, 30, 62, .7);">
+                <div class="modal-header border-0">
+                    <button type="button" class="btn bg-white btn-close" data-bs-dismiss="modal"
+                        aria-label="Close"></button>
+                </div>
+                <div class="modal-body d-flex align-items-center justify-content-center">
+                    <div class="input-group" style="max-width: 600px;">
+                        <input type="text" class="form-control bg-transparent border-primary p-3"
+                            placeholder="Type search keyword">
+                        <button class="btn btn-primary px-4"><i class="bi bi-search"></i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Full Screen Search End -->
+
+
+
+    <div style="padding: 35px;">
+
+        <form enctype="multipart/form-data" method="POST" action="" >
+            <div class="row g-3">
+                <div class="col-md-6">
+                    <div class="form-floating">
+                        <input class="form-control" type="text" name="sender_name" placeholder="Your Name" required />
+                        <label for="name">Your Name</label>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-floating">
+                        <input class="form-control " type="email" name="sender_email" placeholder="Email Address"
+                            required />
+                        <label for="email">Your Email</label>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-floating">
+                        <input type="text" class="form-control" id="phone" name="phone" placeholder="Your Phone Number"
+                            required />
+                        <label for="phone">Your Phone Number</label>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-floating">
+                        <input type="text" class="form-control" id="exp" name="exp" placeholder="Your Experience">
+                        <label for="exp">Your Experience</label>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="form-floating">
+                        <input type="text" class="form-control" id="postt" name="postt" placeholder="Desired Post"
+                            required />
+                        <label for="postt">Desired Post</label>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="form-floating">
+                        <input type="text" class="form-control" id="address" name="address" placeholder="Your Address"
+                            required />
+                        <label for="address">Your Address</label>
+                    </div>
+                </div>
+
+
+                <label for="myfile">Upload Resume:</label>
+                <input type="file" name="attachment" placeholder="Resume" accept=".pdf" required />
+
+
+                <br /><br />
+
+            </div>
+
+
+            <div class="col-12" style="padding: 40px;">
+                <button class="btn btn-primary w-100 py-3" type="submit" name="button" value="Submit">Apply Now</button>
+            </div>
+
+        </form>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Footer Start -->
+    <div class="container-fluid bg-dark text-light mt-5 wow fadeInUp" data-wow-delay="0.1s">
+        <div class="container">
+            <div class="row gx-5">
+                <div class="col-lg-4 col-md-6 footer-about">
+                    <div
+                        class="d-flex flex-column align-items-center justify-content-center text-center h-100 bg-primary p-4">
+                        <a href="index.html" class="navbar-brand">
+                            <h1 class="m-0 text-white"><i class="fa fa-user-tie me-2"></i>CARNOCEAN</h1>
+                        </a>
+                        <p class="mt-3 mb-4">Just because you fail once, doesn’t mean you’re gonna fail at everything.
+                            Keep trying, hold on, and always, always, always believe in yourself, because if you don’t,
+                            then who will??</p>
+                        <form action="">
+                            <div class="input-group">
+                                <input type="text" class="form-control border-white p-3" placeholder="Your Email">
+                                <button class="btn btn-dark">Reach Us</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-lg-8 col-md-6">
+                    <div class="row gx-5">
+                        <div class="col-lg-4 col-md-12 pt-5 mb-5">
+                            <div class="section-title section-title-sm position-relative pb-3 mb-4">
+                                <h3 class="text-light mb-0">Get In Touch</h3>
+                            </div>
+                            <div class="d-flex mb-2">
+                                <i class="bi bi-geo-alt text-primary me-2"></i>
+                                <p class="mb-0">Office No. B-301, 3rd Floor, Moreshwar Apart.,
+                                    Opp. Nerul Railway Station,
+                                    Sec -20, Nerul (W), Navi Mumbai - 400706.</p>
+                            </div>
+                            <div class="d-flex mb-2">
+                                <i class="bi bi-envelope-open text-primary me-2"></i>
+                                <p class="mb-0">info@Carnocean.com</p>
+                            </div>
+                            <div class="d-flex mb-2">
+                                <i class="bi bi-telephone text-primary me-2"></i>
+                                <p class="mb-0">+91 1234567890</p>
+                            </div>
+                            <div class="d-flex mt-4">
+                                <a class="btn btn-primary btn-square me-2" href="#"><i
+                                        class="fab fa-twitter fw-normal"></i></a>
+                                <a class="btn btn-primary btn-square me-2" href="#"><i
+                                        class="fab fa-facebook-f fw-normal"></i></a>
+                                <a class="btn btn-primary btn-square me-2" href="#"><i
+                                        class="fab fa-linkedin-in fw-normal"></i></a>
+                                <a class="btn btn-primary btn-square" href="#"><i
+                                        class="fab fa-instagram fw-normal"></i></a>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-12 pt-0 pt-lg-5 mb-5">
+                            <div class="section-title section-title-sm position-relative pb-3 mb-4">
+                                <h3 class="text-light mb-0">Quick Links</h3>
+                            </div>
+                            <div class="link-animated d-flex flex-column justify-content-start">
+                                <a class="text-light mb-2" href="index.html"><i
+                                        class="bi bi-arrow-right text-primary me-2"></i>Home</a>
+                                <a class="text-light mb-2" href="apply.html"><i
+                                        class="bi bi-arrow-right text-primary me-2"></i>Apply Now</a>
+                                <a class="text-light mb-2" href="about.html"><i
+                                        class="bi bi-arrow-right text-primary me-2"></i>About Us</a>
+                                <a class="text-light mb-2" href="refund.html"><about.html
+                                        class="bi bi-arrow-right text-primary me-2"></about.html>Refund</a>
+                                <a class="text-light mb-2" href="contact.html"><i
+                                        class="bi bi-arrow-right text-primary me-2"></i>Contact Us</a>
+
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-12 pt-0 pt-lg-5 mb-5">
+                            <div class="section-title section-title-sm position-relative pb-3 mb-4">
+                                <h3 class="text-light mb-0">Services</h3>
+                            </div>
+                            <div class="link-animated d-flex flex-column justify-content-start">
+                                <a class="text-light mb-2" href="about.html"><i
+                                        class="bi bi-arrow-right text-primary me-2"></i>Cruise Ship</a>
+                                <a class="text-light mb-2" href="about.html"><i
+                                        class="bi bi-arrow-right text-primary me-2"></i>Shipyard</a>
+                                <a class="text-light mb-2" href="about.html"><i
+                                        class="bi bi-arrow-right text-primary me-2"></i>Cargo Vessels</a>
+                                <a class="text-light mb-2" href="about.html"><i
+                                        class="bi bi-arrow-right text-primary me-2"></i>Seaport</a>
+                                <a class="text-light mb-2" href="about.html"><i
+                                        class="bi bi-arrow-right text-primary me-2"></i>Five Star Hotel Seaport</a>
+                                <a class="text-light" href="about.html"><i class="bi bi-arrow-right text-primary me-2"></i>Oil
+                                    Rig</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid text-white" style="background: #061429;">
+        <div class="container text-center">
+            <div class="row justify-content-end">
+                <div class="col-lg-8 col-md-6">
+                    <div class="d-flex align-items-center justify-content-center" style="height: 75px;">
+                        <p class="mb-0">&copy; <a class="text-white border-bottom" href="/">Carnocean.com</a>. All
+                            Rights Reserved.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+
+
+	
+	<?php
+
+if(isset($_POST['button']) && isset($_FILES['attachment']))
+{
+	$from_email		 = 'jobapply@carnocean.com'; //from mail, sender email address
+	$recipient_email = 'jobapply@carnocean.com'; //recipient email address
+	
+	//Load POST data from HTML form
+	$sender_name = $_POST["sender_name"]; //sender name
+	$reply_to_email = $_POST["sender_email"]; //sender email, it will be used in "reply-to" header
+	$subject	 = "This is Job Apply Form $sender_name"; //subject for the email
+	
+	
+	$phone2 = $_POST["phone"];	
+	$exp2 = $_POST["exp"];	
+	$postt2 = $_POST["postt"];	
+	$address2 = $_POST["address"];	
+	
+	
+		
+// 	echo $phone2;
+// 	echo $postt2;
+// 	echo $address2;
+ //	echo $sender_name;
+	
+	     $message = "Details of the Applicant $sender_name \n";
+         $message .= "Name        : $sender_name \n";
+         $message .= "Email       : $reply_to_email \n";
+         $message .= "Mobile No   : $phone2 \n";
+         $message .= "Experience  : $exp2 \n";
+         $message .= "Desired Post: $postt2 \n";
+         $message .= "Address     : $address2 \n \n";
+		
+
+
+//Always remember to validate the form fields like this
+	if(strlen($sender_name)<1)
+	{
+		 echo "<script>alert('Name is too short or empty!');</alert>";
+ 
+		die;
+	}
+
+	//Get uploaded file data using $_FILES array
+	$tmp_name = $_FILES['attachment']['tmp_name']; // get the temporary file name of the file on the server
+	$name	 = $_FILES['attachment']['name']; // get the name of the file
+	$size	 = $_FILES['attachment']['size']; // get size of the file for size validation
+	$type	 = $_FILES['attachment']['type']; // get type of the file
+	$error	 = $_FILES['attachment']['error']; // get the error (if any)
+
+	//validate form field for attaching the file
+	if($error > 0)
+	{
+		die('Upload error or No files uploaded');
+	}
+
+	//read from the uploaded file & base64_encode content
+	$handle = fopen($tmp_name, "r"); // set the file handle only for reading the file
+	$content = fread($handle, $size); // reading the file
+	fclose($handle);				 // close upon completion
+
+	$encoded_content = chunk_split(base64_encode($content));
+	$boundary = md5("random"); // define boundary with a md5 hashed value
+
+	//header
+	$headers = "MIME-Version: 1.0\r\n"; // Defining the MIME version
+	$headers .= "From:".$from_email."\r\n"; // Sender Email
+	$headers .= "Reply-To: ".$reply_to_email."\r\n"; // Email address to reach back
+	$headers .= "Content-Type: multipart/mixed;"; // Defining Content-Type
+	$headers .= "boundary = $boundary\r\n"; //Defining the Boundary
+	
+	//plain text
+	$body = "--$boundary\r\n";
+	$body .= "Content-Type: text/plain; charset=ISO-8859-1\r\n";
+	$body .= "Content-Transfer-Encoding: base64\r\n\r\n";
+	$body .= chunk_split(base64_encode($message));
+	
+    $body .= chunk_split(base64_encode($message));
+		
+	//attachment
+	$body .= "--$boundary\r\n";
+	$body .="Content-Type: $type; name=".$name."\r\n";
+	$body .="Content-Disposition: attachment; filename=".$name."\r\n";
+	$body .="Content-Transfer-Encoding: base64\r\n";
+	$body .="X-Attachment-Id: ".rand(1000, 99999)."\r\n\r\n";
+	$body .= $encoded_content; // Attaching the encoded file with email
+	
+	$sentMailResult = mail($recipient_email, $subject, $body, $headers);
+
+	if($sentMailResult ){
+	    
+
+	    
+		echo "<script>alert('Your Job Application has been submitted.');</script>";
+	    unlink($name); // delete the file after attachment sent.
+	}
+	else{
+	    
+	    	echo "<script>alert('Sorry but your job application could not be sent. Please go back and try again!');</script>";
+		die;
+	}
+}
+?>
+
+
+
+
+</body>
+
+</html>
