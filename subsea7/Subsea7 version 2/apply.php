27d4e4dc24dc922a93aa98e5a2a67780b728d822
Jatin Tiwari <myemail>
2022-12-21 16:35:37 +0530

    nidhi website

---
+++ subsea7/Subsea7 version 2/apply.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+        <title>SUBSEA7 OFFSHORE OIL AND GAS ENGINEERING CONSTRUCTION INDIA PVT. LTD.</title>
+
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+      <meta content="We Fill The Gap Between Recruiters and Applicants." name="description">
+
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <meta property="og:image" content="https://subseas7ind.com/logo.png">
+
+
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600&family=Rubik:wght@500;600;700&display=swap" rel="stylesheet">
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-border text-primary" role="status" style="width: 3rem; height: 3rem;"></div>
+    </div>
+
+
+   
+
+
+    <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top py-0 pe-5">
+        <a href="index.html" class="navbar-brand ps-1 me-0">
+                      <h3 class="mb-1 text-white"> <span><img src="favicon.ico" style="width:auto; height: 69px; padding-top: 2px;"></span></h3>
+
+        </a>
+        <button type="button" class="navbar-toggler me-0" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto p-4 p-lg-0">
+                <a href="index.html" class="nav-item nav-link active">Home</a>
+                <a href="about.html" class="nav-item nav-link">About</a>
+                 <a href="service.html" class="nav-item nav-link">Services</a>
+            <a href="apply.php" class="nav-item nav-link active">Apply Now</a>
+                 <a href="refund.html" class="nav-item nav-link">Refund</a>
+                <a href="contact.html" class="nav-item nav-link">Contact</a>
+            </div>
+        </div>
+    </nav>
+
+
+   <div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container py-5">
+        <h1 class="display-3 text-white animated slideInRight">Apply Now</h1>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb animated slideInRight mb-0">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item"><a href="#">Apply</a></li>
+            </ol>
+        </nav>
+    </div>
+</div>
+
+ 
+ 
+    <div style="padding: 35px;">
+
+        <form enctype="multipart/form-data" method="POST" action="" >
+            <div class="row g-3">
+                <div class="col-md-6">
+                    <div class="form-floating">
+                        <input class="form-control" type="text" name="sender_name" placeholder="Your Name" required />
+                        <label for="name">Your Name</label>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-floating">
+                        <input class="form-control " type="email" name="sender_email" placeholder="Email Address"
+                            required />
+                        <label for="email">Your Email</label>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-floating">
+                        <input type="text" class="form-control" id="phone" name="phone" placeholder="Your Phone Number"
+                            required />
+                        <label for="phone">Your Phone Number</label>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-floating">
+                        <input type="text" class="form-control" id="exp" name="exp" placeholder="Your Experience">
+                        <label for="exp">Your Experience</label>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="form-floating">
+                        <input type="text" class="form-control" id="postt" name="postt" placeholder="Desired Post"
+                            required />
+                        <label for="postt">Desired Post</label>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="form-floating">
+                        <input type="text" class="form-control" id="address" name="address" placeholder="Your Address"
+                            required />
+                        <label for="address">Your Address</label>
+                    </div>
+                </div>
+
+
+                <label for="myfile">Upload Resume:</label>
+                <input type="file" name="attachment" placeholder="Resume" accept=".pdf" required />
+
+
+                <br /><br />
+
+            </div>
+
+
+            <div class="col-12" style="padding: 40px;">
+                <button class="btn btn-primary w-100 py-3" type="submit" name="button" value="Submit">Apply Now</button>
+            </div>
+
+        </form>
+
+    </div>
+
+
+ 
+    <div class="container-fluid bg-dark footer mt-5 py-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-3 col-md-6">
+                    <h5 class="text-white mb-4">Our Office</h5>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>RBS Business House, 105-A, Satyam Industrial Estate, Station Road, Govandi (E), Mumbai - 400088</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+91 9326923697</p>
+                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@subseas7ind.com</p>
+                    <div class="d-flex pt-3">
+                        <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-youtube"></i></a>
+                        <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h5 class="text-white mb-4">Quick Links</h5>
+                      <a class="btn btn-link" href="/index.html">Home</a>
+                    <a class="btn btn-link" href="/apply.php">Apply Now</a>
+                    <a class="btn btn-link" href="/about.html">About</a>
+                   <a class="btn btn-link" href="/service.html">Services</a>
+                    <a class="btn btn-link" href="/refund.html">Refund</a>
+                    <a class="btn btn-link" href="/contact.html">Contact Us</a>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h5 class="text-white mb-4">Business Hours</h5>
+                    <p class="mb-1">Monday - Saturday</p>
+                    <h6 class="text-light">09:00 am - 07:00 pm</h6>
+                  
+                    <p class="mb-1">Sunday</p>
+                    <h6 class="text-light">Closed</h6>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                                        <p>Sign up today for tips and the latest recruitment news</p>
+
+                    <div class="position-relative w-100">
+                        <input class="form-control bg-transparent w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
+                        <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    
+
+
+    <!-- whatsapp icon link -->
+    <a class="whatsapp_float" href="//api.whatsapp.com/send?phone=919326923697&text=Hello Subseas7, "
+        target="_blank"> <img src="img/whatsapp.jpg" width="49px"></a>
+
+
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+
+    <script src="js/main.js"></script>
+
+
+
+	
+	<?php
+
+if(isset($_POST['button']) && isset($_FILES['attachment']))
+{
+	$from_email		 = 'application@subseas7ind.com'; //from mail, sender email address
+	$recipient_email = 'application@subseas7ind.com'; //recipient email address
+	
+	//Load POST data from HTML form
+	$sender_name = $_POST["sender_name"]; //sender name
+	$reply_to_email = $_POST["sender_email"]; //sender email, it will be used in "reply-to" header
+	$subject	 = "This is Job Apply Form $sender_name"; //subject for the email
+	
+	
+	$phone2 = $_POST["phone"];	
+	$exp2 = $_POST["exp"];	
+	$postt2 = $_POST["postt"];	
+	$address2 = $_POST["address"];	
+	
+	
+		
+// 	echo $phone2;
+// 	echo $postt2;
+// 	echo $address2;
+ //	echo $sender_name;
+	
+	     $message = "Details of the Applicant $sender_name \n";
+         $message .= "Name        : $sender_name \n";
+         $message .= "Email       : $reply_to_email \n";
+         $message .= "Mobile No   : $phone2 \n";
+         $message .= "Experience  : $exp2 \n";
+         $message .= "Desired Post: $postt2 \n";
+         $message .= "Address     : $address2 \n \n";
+		
+
+
+//Always remember to validate the form fields like this
+	if(strlen($sender_name)<1)
+	{
+		 echo "<script>alert('Name is too short or empty!');</alert>";
+ 
+		die;
+	}
+
+	//Get uploaded file data using $_FILES array
+	$tmp_name = $_FILES['attachment']['tmp_name']; // get the temporary file name of the file on the server
+	$name	 = $_FILES['attachment']['name']; // get the name of the file
+	$size	 = $_FILES['attachment']['size']; // get size of the file for size validation
+	$type	 = $_FILES['attachment']['type']; // get type of the file
+	$error	 = $_FILES['attachment']['error']; // get the error (if any)
+
+	//validate form field for attaching the file
+	if($error > 0)
+	{
+		die('Upload error or No files uploaded');
+	}
+
+	//read from the uploaded file & base64_encode content
+	$handle = fopen($tmp_name, "r"); // set the file handle only for reading the file
+	$content = fread($handle, $size); // reading the file
+	fclose($handle);				 // close upon completion
+
+	$encoded_content = chunk_split(base64_encode($content));
+	$boundary = md5("random"); // define boundary with a md5 hashed value
+
+	//header
+	$headers = "MIME-Version: 1.0\r\n"; // Defining the MIME version
+	$headers .= "From:".$from_email."\r\n"; // Sender Email
+	$headers .= "Reply-To: ".$reply_to_email."\r\n"; // Email address to reach back
+	$headers .= "Content-Type: multipart/mixed;"; // Defining Content-Type
+	$headers .= "boundary = $boundary\r\n"; //Defining the Boundary
+	
+	//plain text
+	$body = "--$boundary\r\n";
+	$body .= "Content-Type: text/plain; charset=ISO-8859-1\r\n";
+	$body .= "Content-Transfer-Encoding: base64\r\n\r\n";
+	$body .= chunk_split(base64_encode($message));
+	
+    $body .= chunk_split(base64_encode($message));
+		
+	//attachment
+	$body .= "--$boundary\r\n";
+	$body .="Content-Type: $type; name=".$name."\r\n";
+	$body .="Content-Disposition: attachment; filename=".$name."\r\n";
+	$body .="Content-Transfer-Encoding: base64\r\n";
+	$body .="X-Attachment-Id: ".rand(1000, 99999)."\r\n\r\n";
+	$body .= $encoded_content; // Attaching the encoded file with email
+	
+	$sentMailResult = mail($recipient_email, $subject, $body, $headers);
+
+	if($sentMailResult ){
+	    
+
+	    
+		echo "<script>alert('Your Job Application has been submitted.');</script>";
+	    unlink($name); // delete the file after attachment sent.
+	}
+	else{
+	    
+	    	echo "<script>alert('Sorry but your job application could not be sent. Please go back and try again!');</script>";
+		die;
+	}
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
